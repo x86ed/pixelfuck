a0cfcbf3843102e58867b7aafac2de98c5ba168e
Adam Siegel <tobioatom@gmail.com>
2011-04-21 23:27:30 -0700

    messaging woes

--- js/content.js
+++ js/content.js
@@ -1,9 +1,68 @@
 $(function() {
 		$('body').prepend('<div class="pf-imageOverlay" class="ui-widget-content"><input type="text" id="messager"/></div>')
 		$( "div.pf-imageOverlay" ).draggable();
+		
+		
 		$('input#messager').keyup(function(){ 
 			var sendVal = $(this).val();
-			port = chrome.extension.connect({name: "sayit"});
+			port = chrome.extension.connect({name: "imageObjects"});
 			port.postMessage({value: sendVal});
 		});
+		
+		$(".pf-closebutton.pf-active").click(function(e){
+			var passable = this
+			e.preventDefault();
+			port = chrome.extension.connect({name: "imageObjects"});
+			port.postMessage({remove: $(".pf-closebutton").index(passable)});
+		});
+		
+		$(".pf-visbutton.pf-active").click(function(e){
+			var passable = this
+			e.preventDefault();
+			port = chrome.extension.connect({name: "imageObjects"});
+			port.postMessage({
+				replace: $(".pf-visbutton").index(passable),
+				value:{
+					display : 'none'
+				}
+			});
+		});
+		
+		var setPos = function(oobject){
+			var passable = oobject;
+			port = chrome.extension.connect({name: "imageObjects"});
+			port.postMessage({
+				replace: $(".pf-imageWrapper").index(passable),
+				value:{
+					xPos : $(passable).css('left'),
+					yPos : $(passable).css('top')
+				}
+			});
+		}
+		
+		var setScale = function(oobject){
+			var passable = oobject;
+			port = chrome.extension.connect({name: "imageObjects"});
+			port.postMessage({
+				replace: $(".pf-imageWrapper").index(passable),
+				value:{
+					width : $(passable).css('width'),
+					height : $(passable).css('height')
+				}
+			});
+		}
+		
+		var setCrop = function(oobject){
+			var passable = oobject;
+			port = chrome.extension.connect({name: "imageObjects"});
+			port.postMessage({
+				replace: $(".pf-imageWrapper").index(passable),
+				value:{
+					clipT : $(passable).find('div.pf-imgCrop').css('top'),
+					clipL : $(passable).find('div.pf-imgCrop').css('left'),
+					clipB : $(passable).find('div.pf-imgCrop').css('top') + $(passable).find('div.pf-imgCrop').css('height'),
+					clipR : $(passable).find('div.pf-imgCrop').css('left') + $(passable).find('div.pf-imgCrop').css('width')
+				}
+			});
+		}
 });
--- js/popup.js
+++ js/popup.js
@@ -1,17 +1,29 @@
 $(function() {
-
-$('#texttar').html('poop');
-
-chrome.extension.onConnect.addListener(function(port) {
-
-  console.assert(port.name == "sayit");
-
-  port.onMessage.addListener(function(msg) {
-
-      $('#texttar').html(msg.value);
-
-  });
-
-});
+		//refreshing the image object from the content page
+		//bg = chrome.extension.getBackgroundPage()
+		//$("#pf-newFile").attr('value',String(bg.imageObjects.values[0]["url"]));
+		
+		$("pf-loadButton").click(function(e){
+			e.preventDefault();
+			port = chrome.extension.connect({name: "imageObjects"});
+			port.postMessage({
+				value: {
+					url: $("#pf-newFile").attr('value'),
+					width : "",
+					height: "",
+					xPos : "",
+					yPos : "",
+					zIndex : "",
+					opacity : "",
+					clipT : "",
+					clipR : "",
+					clipB : "",
+					clipL : "",
+					display : ""
+				},
+				remove: "none",
+				replace: "none" 
+			});
+		});
 
 });
